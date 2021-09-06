bbf14a3654e2a6d08c8311cf67978b033f925590
jmerc77 <24909068+jmerc77@users.noreply.github.com>
2021-09-06 01:18:11 -0400

    updates...
    - added 1 new visual
    - added fft output gain adjustment
    - tweaked values

--- sf_oled_audio_visualizer/sf_oled_audio_visualizer.ino
+++ sf_oled_audio_visualizer/sf_oled_audio_visualizer.ino
@@ -16,16 +16,17 @@ const uint16_t samples = 128;//2 * screen_width
 double vImag[samples], vReal[samples];
 const double rainspdmin = 0.2;
 const double slimespdmin = 0.2;
-const double rainspdmax = 6.0;
-const double slimespdmax = 3.0;
+const double rainspdmax = 16.0;
+const double slimespdmax = 8.0;
 const double barspd = 0.5;
-const uint8_t greys = 16;
-const int uspersamp = 300;
+const double fftvol = 0.25;
+const uint8_t greys = 32;
+const int uspersamp = 260;
 uint8_t exponent;
 volatile unsigned int samp = 0, t2 = 20000;
 volatile float rainbuff[sw], rainbuff2[sw];
 volatile uint8_t buff[sw][sh];
-volatile int sel = 2, hitbottom = sw, y = 0;
+volatile int sel = 3, hitbottom = sw, y = 0;
 elapsedMillis t = 0;
 MicroOLED oled(PIN_RESET, PIN_DC, PIN_CS);
 arduinoFFT FFT = arduinoFFT();
@@ -61,35 +62,66 @@ void getsamp()
   {
     if (t > t2)
     {
-      sel = random(9);//(sel + 1) % 9;
-      t2 = random(20000) + 15000;
+      //clr midi's buffer
+      if(sel==0)
+      {
+        for (int i = 0; i < sh; i++)
+      {
+        for (int i2 = 0; i2 < sw; i2++)
+        {
+          buff[i2][i]=0;
+        }
+      }
+      }
+      sel = random(10);//(sel + 1) % 10;//pick new fx
+      t2 = random(20000) + 15000;//pick new duration
       hitbottom = 0;
       t = 0;
     }
     switch (sel)
     {
       case 0:
+        //midi
+        FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
+        FFT.ComplexToMagnitude(vReal, vImag, samples);
+        //compute min/max
+        fftlow = vReal[0];
+        ffthigh = vReal[0];
+        for (int i = 1; i < sw; i++)
+        {
+          if (vReal[i] > ffthigh)ffthigh = vReal[i];
+          if (vReal[i] < fftlow)fftlow = vReal[i];
+        }
+        //put in buffer
+        for (int i = 0; i < sw; i++)
+        {
+          buff[i][y] = (uint8_t)map(vReal[i], fftlow, ffthigh, 0, greys - 1); //>midithresh;
+        }
+        //update y pos
+        y = (y + sh - 1) % sh;
+        break;
+      case 1:
         //rain
         FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, samples);
         oled.clear(PAGE);
         for (int i = 0; i < sw; i++)
         {
-          rainbuff[i] = (rainbuff[i] + vReal[i] * rainspdmax / 511.0 + rainspdmin); //+rainspdmax/2.0;
+          rainbuff[i] = (rainbuff[i] + vReal[i] * rainspdmax / 255.0 * fftvol + rainspdmin); //+rainspdmax/2.0;
           if (rainbuff[i] > sh - 1)rainbuff[i] -= sh - 1;
           rainbuff2[i] = rainbuff[i];
           oled.pixel(i, rainbuff[i]);
         }
         oled.display();
         break;
-      case 1:
+      case 2:
         //bars
         FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, samples);
         oled.clear(PAGE);
         for (int i = 0; i < sw; i++)
         {
-          int bar = sh - 1 - map(constrain(vReal[i], 0, 511), 0, 511, 0, sh - 1);
+          int bar = sh - 1 - map(constrain(vReal[i] * fftvol, 0, 255), 0, 255, 0, sh - 1);
           rainbuff2[i] = bar;
           rainbuff[i] += barspd;
           if (rainbuff[i] > bar)rainbuff[i] = bar;
@@ -98,7 +130,7 @@ void getsamp()
         }
         oled.display();
         break;
-      case 2:
+      case 3:
         //osc
         oled.clear(PAGE);
         for (int i = 0; i < sw; i++)
@@ -114,7 +146,7 @@ void getsamp()
         }
         oled.display();
         break;
-      case 3:
+      case 4:
         //slime
         FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, samples);
@@ -126,7 +158,7 @@ void getsamp()
           if (rst)rainbuff2[i] = 0;
           if (rainbuff2[i] <= sh - 1)
           {
-            rainbuff2[i] = (rainbuff2[i] + vReal[i] * slimespdmax / 511.0 + slimespdmin);
+            rainbuff2[i] = (rainbuff2[i] + vReal[i] * slimespdmax / 255.0 * fftvol + slimespdmin);
           }
           else
           {
@@ -152,9 +184,10 @@ void getsamp()
         }
         oled.display();
         break;
-      case 4:
+      case 5:
         //osc and rain
         oled.clear(PAGE);
+        //osc
         for (int i = 0; i < sw; i++)
         {
           if (i > 0)
@@ -165,17 +198,18 @@ void getsamp()
           }
           rainbuff2[i] = sh - 1 - constrain(((vReal[i * 2] + 128) * (sh - 1) / 255.0), 0, sh - 1);
         }
+        //rain
         FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, samples);
         for (int i = 0; i < sw; i++)
         {
-          rainbuff[i] = (rainbuff[i] + vReal[i]  * rainspdmax / 511.0 + rainspdmin); //+rainspdmax/2.0;
+          rainbuff[i] = (rainbuff[i] + vReal[i]  * rainspdmax / 255.0 * fftvol + rainspdmin); //+rainspdmax/2.0;
           if (rainbuff[i] > sh - 1)rainbuff[i] -= sh - 1;
           oled.pixel(i, rainbuff[i]);
         }
         oled.display();
         break;
-      case 5:
+      case 6:
         //slime and rain
         FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, samples);
@@ -187,7 +221,7 @@ void getsamp()
           if (rst)rainbuff2[i] = 0;
           if (rainbuff2[i] <= sh - 1)
           {
-            rainbuff2[i] = (rainbuff2[i] + vReal[i] * slimespdmax / 511.0 + slimespdmin);
+            rainbuff2[i] = (rainbuff2[i] + vReal[i] * slimespdmax / 255.0 * fftvol + slimespdmin);
           }
           else
           {
@@ -209,40 +243,92 @@ void getsamp()
               oled.line(i - 1, (uint8_t)floor(rainbuff2[i - 1]), i, sh - 1);
             }
           }
-          rainbuff[i] = (rainbuff[i] + vReal[i]  * rainspdmax / 511.0 + rainspdmin); //+rainspdmax/2.0;
+          rainbuff[i] = (rainbuff[i] + vReal[i]  * rainspdmax / 255.0 * fftvol + rainspdmin); //+rainspdmax/2.0;
           if (rainbuff[i] > sh - 1)rainbuff[i] -= sh - 1;
           oled.pixel(i, rainbuff[i]);
         }
         oled.display();
         break;
-      case 6:
-        //midi
+      case 7:
+        //bouncy
         FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, samples);
-        //compute min/max
-        fftlow = vReal[0];
-        ffthigh = vReal[0];
-        for (int i = 1; i < sw; i++)
-        {
-          if (vReal[i] > ffthigh)ffthigh = vReal[i];
-          if (vReal[i] < fftlow)fftlow = vReal[i];
-        }
-        //put in buffer
+        oled.clear(PAGE);
         for (int i = 0; i < sw; i++)
         {
-          buff[i][y] = (uint8_t)map(vReal[i], fftlow, ffthigh, 0, greys - 1); //>midithresh;
+          if (buff[i][0])
+          {
+            rainbuff[i] = (rainbuff[i] - vReal[i]  * rainspdmax / 255.0 * fftvol - rainspdmin); //+rainspdmax/2.0;
+            if (rainbuff[i] < 0)
+            {
+              rainbuff[i] = -rainbuff[i];
+              buff[i][0] = false;
+            }
+          }
+          else
+          {
+            rainbuff[i] = (rainbuff[i] + vReal[i]  * rainspdmax / 255.0 * fftvol + rainspdmin); //+rainspdmax/2.0;
+            if (rainbuff[i] > sh - 1)
+            {
+              rainbuff[i] = sh - 1 - rainbuff[i] + sh - 1;
+              buff[i][0] = true;
+            }
+          }
+          rainbuff2[i] = rainbuff[i];
+          oled.pixel(i, rainbuff[i]);
         }
-        //update y pos
-        y = (y + sh - 1) % sh;
+        oled.display();
         break;
-      case 7:
+      case 8:
+          //osc and bouncy
+        oled.clear(PAGE);
+        //osc
+        for (int i = 0; i < sw; i++)
+        {
+          if (i > 0)
+          {
+            uint8_t point1 = sh - 1 - constrain(((vReal[i * 2 - 2] + 128) * (sh - 1) / 255.0), 0, sh - 1);
+            uint8_t   point2 = sh - 1 - constrain(((vReal[i * 2] + 128) * (sh - 1) / 255.0), 0, sh - 1);
+            oled.line(i - 1, point1, i, point2);
+          }
+          rainbuff2[i] = sh - 1 - constrain(((vReal[i * 2] + 128) * (sh - 1) / 255.0), 0, sh - 1);
+        }
+        //bouncy
+        FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
+        FFT.ComplexToMagnitude(vReal, vImag, samples);
+        for (int i = 0; i < sw; i++)
+        {
+          if (buff[i][0])
+          {
+            rainbuff[i] = (rainbuff[i] - vReal[i]  * rainspdmax / 255.0 * fftvol - rainspdmin); //+rainspdmax/2.0;
+            if (rainbuff[i] < 0)
+            {
+              rainbuff[i] = -rainbuff[i];
+              buff[i][0] = false;
+            }
+          }
+          else
+          {
+            rainbuff[i] = (rainbuff[i] + vReal[i]  * rainspdmax / 255.0 * fftvol + rainspdmin); //+rainspdmax/2.0;
+            if (rainbuff[i] > sh - 1)
+            {
+              rainbuff[i] = sh - 1 - rainbuff[i] + sh - 1;
+              buff[i][0] = true;
+            }
+          }
+          rainbuff2[i] = rainbuff[i];
+          oled.pixel(i, rainbuff[i]);
+        }
+        oled.display();
+        break;
+      case 9:
         //jaw
         FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
         FFT.ComplexToMagnitude(vReal, vImag, samples);
         oled.clear(PAGE);
         for (int i = 0; i < sw; i++)
         {
-          int bar = map(constrain(vReal[i], 0, 511), 0, 511, 0, sh - 1);
+          int bar = map(constrain(vReal[i] * fftvol, 0, 255), 0, 255, 0, sh - 1);
           rainbuff2[i] = bar;
           if (i % 2 == 0)
           {
@@ -262,41 +348,12 @@ void getsamp()
         }
         oled.display();
         break;
-      case 8:
-        //bouncy
-        FFT.Compute(vReal, vImag, samples, exponent, FFT_FORWARD);
-        FFT.ComplexToMagnitude(vReal, vImag, samples);
-        oled.clear(PAGE);
-        for (int i = 0; i < sw; i++)
-        {
-          if (buff[i][0])
-          {
-            rainbuff[i] = (rainbuff[i] - vReal[i]  * rainspdmax / 511.0 - rainspdmin); //+rainspdmax/2.0;
-            if (rainbuff[i] < 0)
-            {
-              rainbuff[i] = -rainbuff[i];
-              buff[i][0] = false;
-            }
-          }
-          else
-          {
-            rainbuff[i] = (rainbuff[i] + vReal[i]  * rainspdmax / 511.0 + rainspdmin); //+rainspdmax/2.0;
-            if (rainbuff[i] > sh - 1)
-            {
-              rainbuff[i] = sh - 1 - rainbuff[i] + sh - 1;
-              buff[i][0] = true;
-            }
-          }
-          rainbuff2[i] = rainbuff[i];
-          oled.pixel(i, rainbuff[i]);
-        }
-        oled.display();
-        break;
+      
     }
   }
-  if (sel == 6)
+  if (sel == 0)
   {
-    if (samp % greys == 0)
+    if ((samp>>1) % greys == 0)
     {
       oled.clear(PAGE);
       for (int i = 0; i < sh; i++)
@@ -313,7 +370,7 @@ void getsamp()
       {
         for (int i2 = 0; i2 < sw; i2++)
         {
-          if (buff[i2][(y + i + 1) % sh] == samp % greys)oled.pixel(i2, i, BLACK, NORM);
+          if (buff[i2][(y + i + 1) % sh] == (samp>>1) % greys)oled.pixel(i2, i, BLACK, NORM);
         }
       }
     }
